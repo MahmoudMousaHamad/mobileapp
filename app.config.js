12ad6a392b10bc6f69321610356364cd9cdbc9bf
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2022-07-21 18:57:06 -0500

    init

--- app.config.js
+++ app.config.js
@@ -2,13 +2,13 @@ const LOCAL_IP_ADDRESS = require("ip").address().toString();
 
 export default {
     name: 'mobileapp',
-    version: '1.0.1',
+    version: '1.0.3',
     owner: "mahmoudmousahamad",
     extra: {
       LOCAL_IP_ADDRESS,
     },
     android: {
       package: "com.jobapplier.app",
-      versionCode: 2,
+      versionCode: 3,
     }
 };
