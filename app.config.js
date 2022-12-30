07aebb682c7c761515dfbf67928d5a34b1759911
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2022-12-29 22:20:39 -0600

    update config, icons, and splash

--- app.config.js
+++ app.config.js
@@ -1,21 +1,45 @@
 const LOCAL_IP_ADDRESS = require("ip").address().toString();
 
 export default {
-    // name: 'mobileapp',
-    // slug: "JobApplier",
-    version: '1.0.7',
+    name: 'JobApplier',
+    slug: "mobileapp",
+    version: '1.0.13',
     owner: "mahmoudmousahamad",
     extra: {
       LOCAL_IP_ADDRESS,
     },
+    sdkVersion: "45.0.0",
+    icon: "./assets/icon.png",
+    backgroundColor: "#0C6CDC",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#0C6CDC"
+    },
+    ios: {
+      bundleIdentifier: "com.useapplier.jobapplier",
+      "supportsTablet": true
+    },
     android: {
       package: "com.jobapplier.app",
-      versionCode: 7,
+      versionCode: 13,
       googleServicesFile: "./google-services.json",
       permissions: ["RECEIVE_BOOT_COMPLETED"],
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#FFFFFF"
-      }
-    }
+      "adaptiveIcon": {
+        "backgroundImage": "./assets/icon.png",
+        "foregroundImage": "./assets/icon.png",
+        "backgroundColor": "#0C6CDC",
+      },
+    },
+    "plugins": [
+      [
+        "expo-notifications",
+        {
+          icon: "./assets/icon.png",
+          color: "#0C6CDC",
+          mode: "production",
+          sounds: [],
+        }
+      ]
+    ],
 };
